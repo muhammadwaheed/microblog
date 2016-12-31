861d50cbae0bbed1838fcedde3f8f9af259f9e49
Muhammad Waheed <muhwahee@cisco.com>
2016-12-31 03:49:00 -0800

    loop in templates

--- app/views.py
+++ app/views.py
@@ -4,5 +4,6 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username' : 'fake'} #fake user
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'fake'} #fake user
+    posts = [{'author': {'nickname': 'Mike'},'body': 'Beautiful day'}, {'author': {'nickname': 'Choi'},'body': 'Success Reloading ...'}]
+    return render_template('index.html', title='Home', user=user, posts=posts)
