f31bb07570c25b5b60922ffbbc4c4524cff8ef35
muhammadwaheed <muhammad.waheed1993@gmail.com>
2017-01-10 03:55:22 -0800

    Form templates and views

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,3 +8,9 @@ def index():
     user = {'username': 'fake'} #fake user
     posts = [{'author': {'nickname': 'Mike'},'body': 'Beautiful day'}, {'author': {'nickname': 'Choi'},'body': 'Success Reloading ...'}]
     return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form= LoginForm()
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
