10ba92fbca143f1ce4e7353b1c7539e7f5a919f1
muhammadwaheed <muhammad.waheed1993@gmail.com>
2017-01-10 09:07:49 -0800

    Receiving form data

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
@@ -6,12 +6,12 @@ from .forms import LoginForm
 @app.route('/index')
 def index():
     user = {'username': 'fake'} #fake user
-    posts = [{'author': {'nickname': 'Mike'},'body': 'Beautiful day'}, {'author': {'nickname': 'Choi'},'body': 'Success Reloading ...'}]
+    posts = [{'author': {'nickname': 'Mike'}, 'body': 'Beautiful day'}, {'author': {'nickname': 'Choi'},'body': 'Success Reloading ...'}]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form= LoginForm()
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form)
+    if form.validate_on_submit():
+        flash('Login Requested for OPENID="%s", remember_me="%s" ' % (form.openid.data, str(form.remember_me.data)))
+    return render_template('login.html', title='Sign In', form=form)
