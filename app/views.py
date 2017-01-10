33a09ca6061d943d63fd17a2787b3caa5f66e70e
muhammadwaheed <muhammad.waheed1993@gmail.com>
2017-01-10 09:30:17 -0800

    Improving field validation

--- app/views.py
+++ app/views.py
@@ -14,4 +14,5 @@ def login():
     form= LoginForm()
     if form.validate_on_submit():
         flash('Login Requested for OPENID="%s", remember_me="%s" ' % (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
