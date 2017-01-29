4a7fae572888180a91327eab7e87805a897c3d0a
muhammadwaheed <muhammad.waheed1993@gmail.com>
2017-01-29 04:53:22 -0800

    update login fuction

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManger
+from flask_login import LoginManager
 from flask_openid import OpenID
 from config import basedir
 
-lm = LoginManger()
+lm = LoginManager()
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
