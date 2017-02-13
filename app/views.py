3929873ce9cdeb4021f418e9c866cf2d6e6e5f3e
muhammadwaheed <waheed@wanclouds.net>
2017-02-13 16:51:35 +0500

    flask-openID login callback

--- app/views.py
+++ app/views.py
@@ -26,3 +26,23 @@ def login():
 def load_user(id):
     return User.query.get(int(id))
 
+@oid.after_login
+def after_login(resp):
+
+    if resp.email is None or resp.email=='':
+        flash("Invalid login. Please try again")
+        redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == '':
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email= resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me =False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    load_user(user, remember_me=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
